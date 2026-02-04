1981950906476d079808927e10ac1192fd158478
Admin <amolujagare@gmail.com>
2026-02-04 22:23:39 +0530

    Java

--- src/StaticNFinal/Counter.java
+++ src/StaticNFinal/Counter.java
@@ -3,17 +3,17 @@ package StaticNFinal;
 public class Counter {
 
    static int count = 0;
-
+   int x;
     Counter()
     {
-        count++;
+      //  count++;
         System.out.println("count="+count);
     }
 
     public static void main(String[] args) {
 
-        Counter c1 = new Counter(); // 1
-        Counter c2 = new Counter(); // 1
-        Counter c3 = new Counter(); // 1
+        Counter c1 = new Counter(); // 0
+        Counter c2 = new Counter(); // 0
+        Counter c3 = new Counter(); // 0
     }
 }
